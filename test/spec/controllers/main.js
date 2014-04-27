1b266d41877c2df69c8dd0cf67fc4d22eed8425a
Nikolay Norkin <n.e.norkin@gmail.com>
2014-04-27 16:08:29 +0400

    sections mocked

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,13 +3,17 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('ngTestingApp'));
+  beforeEach(module('ngTestingApp', 'SectionsMock'));
 
   var MainCtrl,
-    scope;
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, defaultJSON) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.whenGET('data/sections.json').respond(defaultJSON);
+
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
@@ -17,12 +21,17 @@ describe('Controller: MainCtrl', function () {
   }));
 
   it('should attach a list of sections to the scope', function () {
+    expect(scope.sections.length).toBe(0);
+    $httpBackend.flush();
     expect(scope.sections.length).toBe(5);
   });
   it('should assign currentSection', function () {
+    expect(scope.currentSection).toBeUndefined();
+    $httpBackend.flush();
     expect(scope.currentSection).not.toBe(null);
   });
   it('initial currentSection must be year', function () {
+    $httpBackend.flush();
     expect(scope.currentSection.id).toBe('year');
   });
   it('should have needed functions', function () {
@@ -36,28 +45,6 @@ describe('Controller: MainCtrl', function () {
   });
 
   describe('controller methods', function () {
-    describe('inc/dec', function () {
-      var prevValue;
-      beforeEach(function () {
-        prevValue = scope.currentSection.value;
-      });
-      it('should incrementCurrentSection', function () {
-        scope.incrementCurrentSection();
-        expect(scope.currentSection.value).not.toBe(prevValue);
-      });
-      it('should decrementCurrentSection', function () {
-        scope.decrementCurrentSection();
-        expect(scope.currentSection.value).not.toBe(prevValue);
-      });
-    });
-    describe('getControlTypeUrl', function () {
-      it('should return proper url', function () {
-        expect(scope.getControlTypeUrl('number')).toMatch(/number.html/);
-      });
-      it('should return default url', function () {
-        expect(scope.getControlTypeUrl()).toMatch(/[a-z]?.html/);
-      });
-    });
     describe('isHasValidationError', function () {
       it('should return true when errors present', function () {
         expect(scope.isHasValidationError({
